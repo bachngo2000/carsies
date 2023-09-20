bef58e069a8529d520242fa5809e7bd46387df12
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-09-20 15:12:14 -0500

    paginated the search results

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -10,10 +10,9 @@ public class SearchController : ControllerBase
 {   
     // And inside this particular controller we don't need to provide a constructor, we don't need to inject the MongoDB entities that we're using. We can simply just use it because it's a static class.
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, int pageNumber = 1, int pageSize = 4)
     {   
-        // use Find to return a full list of items w/o a search term
-        var query = DB.Find<Item>();
+        var query = DB.PagedSearch<Item>();
 
         // sort by the name of the Make
         query.Sort(x => x.Ascending(a => a.Make));
@@ -22,9 +21,17 @@ public class SearchController : ControllerBase
             query.Match(Search.Full, searchTerm).SortByTextScore();
         }
 
+        query.PageNumber(pageNumber);
+        query.PageSize(pageSize);
+
         var result = await query.ExecuteAsync();
 
-        return result;
+        return Ok(new
+        {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
     }
 
 }
