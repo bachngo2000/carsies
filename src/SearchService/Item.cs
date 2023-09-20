3826187df3281a538ebd86670a11da1f356f8c92
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-09-20 11:03:20 -0500

    defined the Item model

---
+++ src/SearchService/Item.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Entities;
+
+namespace SearchService.Models;
+public class Item : Entity
+{   
+    // The Entity class provides an ID for our item and effectively in our MongoDB database, we're going to have collections of documents
+    // and for this Item : Entit, because we've derived from Entity, when we do initialize our database, this is considered an entity class now inside MongoDB 
+    // and we don't actually need to give it its own ID because this is coming from entity
+
+    public int ReservePrice{get; set;} = 0;
+
+    public string Seller{get; set;}
+
+    public string Winner{get; set;}
+
+    public int SoldAmount{get; set;}
+
+    public int CurrentHighBid{get; set;}
+
+    public DateTime CreatedAt {get; set;} 
+
+    public DateTime UpdatedAt {get; set;}
+
+    public DateTime AuctionEnd {get; set;}
+
+    public string Status {get; set;}
+
+    public string Make {get; set;}
+
+    public string Model {get; set;}
+
+    public int Year {get; set;}
+
+    public string Color {get; set;}
+
+    public int Mileage {get; set;}
+
+    public string ImageUrl {get; set;}
+
+}
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,5 +15,16 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// initialize our MongoDB database
+await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+// What we're also going to do, because this is going to act as a search server, is we're going to create an index for our item for the 
+// certain fields that we want to be able to search on.
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
