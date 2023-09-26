409132a270da761646cfc2bd3ea3a7f11be67464
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-09-26 15:01:12 -0500

    added the remaining consumers (AuctionFinishedConsumer and BidPlacedConsumer) in the Search service

---
+++ src/SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,29 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService;
+public class AuctionFinishedConsumer: IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        // get the specific auction
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        // check if the auction has been sold
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = (int)context.Message.Amount;
+        }
+
+        // update auction status
+        auction.Status = "Finished";
+
+        // save everything to the Mongo database
+        await auction.SaveAsync();
+    }
+
+
+}
