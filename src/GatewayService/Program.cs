3ea82787af1b236f7bc355f57f813613fee39e89
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-09-26 12:52:23 -0500

    added authentication to the Gateway configuration

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,10 +1,23 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //add the configuration we need for the reverse proxy using Yarp into our program class
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
 var app = builder.Build();
 
 app.MapReverseProxy();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
