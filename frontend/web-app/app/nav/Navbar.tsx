2fc472d9c32054d23b4469f62f8484c2c45322f7
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-09-29 15:54:21 -0500

    implemented the search functionality

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,9 +1,8 @@
 // {/* inform Next.js that this is a client side component, not server side*/}
 // 'use client'
-import {AiOutlineCar} from 'react-icons/ai';
-
 import React from 'react'
 import Search from './Search';
+import Logo from './Logo';
 
 export default function Navbar() {
   return (
@@ -11,13 +10,12 @@ export default function Navbar() {
     // we want our header to stick to the top of the screen, above all other contents, and our divs to be displayed in line and to spread across the top of our browser window
     // white background, padding all around, center things horizontally, and text formatting
     <header className='sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md'>
-      <div>
-        <div className='flex items-center gap-2 text-3xl font-semibold text-red-500'>
-          {/* added icon from the react icons library*/}
+        {/* we want to be able to click on the name Carsies Auctions and reset our page. Now, in order to use the reset function, that means using our params store because that is a react hook that needs to be done inside a client component. I don't want to change the nav bar from a server component to a client component, so I'm going to create a new component, called Logo.tsx, just for this which will allow us to reset our state./*}
+        {/*<div className='flex items-center gap-2 text-3xl font-semibold text-red-500'>
           <AiOutlineCar size={34}/>
           Carsies Auctions
-        </div>
-      </div>
+        </div>*/}
+      <Logo/>
       <Search/>
       <div> Login </div>
     </header>
--- frontend/web-app/app/nav/Search.tsx
+++ frontend/web-app/app/nav/Search.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+'use client'
+import { useParamsStore } from '@/hooks/useParamsStore'
+import React, { useState } from 'react'
 import {FaSearch} from 'react-icons/fa'
 
 export default function Search() {
+
+  // get setParams function from the store
+  const setParams = useParamsStore(state => state.setParams);
+
+  // using local state bc we need to track the state of what is being typed into the input field
+  const [value, setValue] = useState('');
+
+  // function to track when the input is being typed into
+  function onChange(event: any) {
+    // event.target.value is the characters typed into the input field
+    setValue(event.target.value);
+  }
+
+  // function to submit the change
+  function search() {
+    // when this function is called, it updates our parameters, and inside our Listings.tsx components, then the URL that the useEffect() function depends on, our useEffect() 
+    // is going to be called whenever this URL changes. And because we're getting our params inside here from the useParamsStore, this is going to react to the searchTerm changing, which means our URL is going to change, which means the useEffect() is going to be called with that updated URL, which will then in turn of course make the request to our search service and get the updated results back.
+    setParams({searchTerm: value});
+  }
+
   return (
     <div className='flex w-[50%] items-center border-2 rounded-full py-2 shadow-sm'>
         <input
+            onKeyDown={(e: any) => {
+              if (e.key == 'Enter') {
+                search()
+              }
+            }}
+            onChange={onChange}
             type="text'"
             placeholder='Search for cars by make, model, or color'
             className='
@@ -19,7 +47,7 @@ export default function Search() {
               text-gray-600
             '
         />
-        <button>
+        <button onClick={search}>
             <FaSearch size={34} className='bg-red-400 text-white rounded-full p-2 cursor-pointer mx-2'></FaSearch>
         </button>
         
