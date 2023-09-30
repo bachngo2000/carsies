150d1ff38043ad8c8a7cb8678576305451e0618f
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-09-30 09:38:58 -0500

    protected routes when an unauthorized user attempts to access the session route

--- frontend/web-app/app/components/EmptyFilter.tsx
+++ frontend/web-app/app/components/EmptyFilter.tsx
@@ -1,14 +1,18 @@
+'use client'
 import { useParamsStore } from '@/hooks/useParamsStore'
 import React from 'react'
 import Heading from './Heading'
 import { Button } from 'flowbite-react'
+import { signIn } from 'next-auth/react'
 
 type Props = {
     title?: string
     subtitle?: string
     showReset?: boolean
+    showLogin?: boolean
+    callbackUrl?: string
 }
-export default function EmptyFilter({title = 'No matches for this filer', subtitle = 'Try changing or resetting the filter', showReset}: Props) {
+export default function EmptyFilter({title = 'No matches for this filer', subtitle = 'Try changing or resetting the filter', showReset, showLogin, callbackUrl}: Props) {
 
     const reset = useParamsStore(state => state.reset);
 
@@ -18,6 +22,9 @@ export default function EmptyFilter({title = 'No matches for this filer', subtit
         <div className='mt-4'>
             {showReset && (
                 <Button outline onClick={reset}> Remove Filters </Button>
+            )}
+            {showLogin && (
+                <Button outline onClick={() => signIn('id-server', {callbackUrl})}>Login</Button>
             )}
         </div>
     </div>
