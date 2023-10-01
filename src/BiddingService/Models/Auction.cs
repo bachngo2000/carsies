45fca7f91f81f2346c0506144947ada5f822dd85
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-10-01 15:21:16 -0500

    added models for the Bid service

---
+++ src/BiddingService/Models/Auction.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Entities;
+
+namespace BiddingService;
+public class Auction : Entity
+{
+    // create some properties that our Bidding service needs to know about for our auction
+    public DateTime AuctionEnd {get; set;}
+
+    public string Seller {get; set;}
+
+    public int ReservePrice {get; set;}
+
+    public bool Finished {get; set;}
+
+}
