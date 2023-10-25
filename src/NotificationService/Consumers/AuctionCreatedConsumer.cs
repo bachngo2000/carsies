9d5174d40579906b7f29800f7d367fe53a8944b4
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-10-24 20:53:44 -0500

    added consumers for AuctionCreated, AuctionFinished, and BidPlaced events

---
+++ src/NotificationService/Consumers/AuctionCreatedConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+
+namespace NotificationService;
+
+public class AuctionCreatedConsumer : IConsumer<AuctionCreated>
+{
+    private readonly IHubContext<NotificationHub> _hubContext;
+
+    // inject the IHubContext
+    public AuctionCreatedConsumer(IHubContext<NotificationHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public async Task Consume(ConsumeContext<AuctionCreated> context)
+    {
+        Console.WriteLine("--> auction created message received");
+
+        // send to all clients
+        await _hubContext.Clients.All.SendAsync("AuctionCreated", context.Message);
+    }
+}
