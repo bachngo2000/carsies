a734b440f89dd31870dd8307f6fa2f4d140dee74
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-10-25 20:14:06 -0500

    updated header

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -22,7 +22,8 @@ export default function Logo() {
     <div onClick={doReset} className='cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500'>
         {/* added icon from the react icons library*/}
         <AiOutlineCar size={34}/>
-        Bach's Car Auctions
+          CarAuctions
     </div>
   )
 }
+
